02d6b1c77b4d20c7176f56185b779e82c532c9e9
AdarshMitr <adarshkumar019@gmail.com>
2023-10-14 11:55:20 +0530

    created primary structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "./Img/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>doOne- an enhanced todo app for you</h1>
+      <div className="header">
+        <img className="logo" src={logo} alt="doOne logo" />
+        <div className="date">
+          Today :{new Date().getDate()}/{new Date().getMonth() + 1}/
+          {new Date().getFullYear()}
+        </div>
+      </div>
+      <div className="body">
+        <div className="form">
+          <input className="input-box" placeholder="write here..." type="text"/>
+          <button className="submit">Add</button> 
+        </div>
+        <div className="task-status">
+          <button className="btn-status">Remaining</button>
+          <button className="btn-status ">Completed</button>
+          <button className="btn-status">All</button>
+        </div>
+      </div>
     </div>
   );
 }
