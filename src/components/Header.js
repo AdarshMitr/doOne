989c605975e6e0014177c3d477d367bb0ce0be10
AdarshMitr <adarshkumar019@gmail.com>
2023-10-16 13:08:24 +0530

    adding some more styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,12 @@ import logo from "../Img/logo.png";
 const Header = () => {
   return (
     <div className="header">
-        <img className="logo" src={logo} alt="doOne logo" />
+      <img className="logo" src={logo} alt="doOne logo" />
         <div className="date">
           Today :{new Date().getDate()}/{new Date().getMonth() + 1}/
           {new Date().getFullYear()}
         </div>
+       
       </div>
   )
 }
